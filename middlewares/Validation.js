2176ac204e9cf6aac8fae07bedbe3613c84e1ef6
Maxim Tarahov <tarahovmaksim@yandex.ru>
2022-04-07 21:20:29 +0400

    routes and validation fix

--- middlewares/Validation.js
+++ middlewares/Validation.js
@@ -42,7 +42,7 @@ const validationSignUp = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
   }),
 });
 
