d76a2430c6278b3cc66a82a0d14baa2e41aae4ca
Maxim Tarahov <tarahovmaksim@yandex.ru>
2022-06-02 19:38:09 +0400

    testing cors

--- app.js
+++ app.js
@@ -11,9 +11,29 @@ const limiter = require('./middlewares/limiter');
 
 const { PORT = 3001, NODE_ENV, MONGO = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const app = express();
-app.use(cors({
-  origin: true,
-}));
+const allowedDomains = [
+  'https://diploma.frontend.nomoredomains.xyz/',
+  'http://diploma.frontend.nomoredomains.xyz/',
+  'http://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedDomains.includes(origin)) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', origin);
+    const { method } = req;
+    const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE';
+    if (method === 'OPTIONS') {
+      const requestHeaders = req.headers['access-control-request-headers'];
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+    }
+  }
+  next();
+});
+
+app.use(cors());
 
 mongoose
   .connect(NODE_ENV === 'production' ? MONGO : 'mongodb://localhost:27017/moviesdb', {
